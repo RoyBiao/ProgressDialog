cc60066ac168c06325dbc263a57c69241ef18541
ruibiao.li <liruibiao@HSliveIM.com>
2015-07-16 16:46:55 +0800

    merge

--- DiaryProgress/src/cn/ry/diary/demo2/Demo2Activity.java
+++ DiaryProgress/src/cn/ry/diary/demo2/Demo2Activity.java
@@ -8,9 +8,10 @@ import cn.ry.diary.R;
 import cn.ry.diary.demo2.common.adapter.CommonAdapterActivity;
 import cn.ry.diary.demo2.guideview.GuideViewActivity;
 import cn.ry.diary.demo2.guideview2.GuideView2Activity;
+import cn.ry.diary.demo2.imageWidget.ImagesWidgetConfiguration;
 import cn.ry.diary.demo2.inflater.LayoutInflaterActivity;
 
-public class Demo2Activity extends Activity{
+public class Demo2Activity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +19,8 @@ public class Demo2Activity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_demo2);
 	}
-	
-	public  void onClickItem(View view) {
+
+	public void onClickItem(View view) {
 		switch (view.getId()) {
 		case R.id.commonAdapterBt:
 			startActivity(new Intent(this, CommonAdapterActivity.class));
@@ -31,6 +32,9 @@ public class Demo2Activity extends Activity{
 			startActivity(new Intent(this, GuideViewActivity.class));
 		case R.id.guideView2Bt:
 			startActivity(new Intent(this, GuideView2Activity.class));
+			break;
+		case R.id.ImagesWidgetConfigurationBt:
+			startActivity(new Intent(this, ImagesWidgetConfiguration.class));
 			break;
 		default:
 			break;
